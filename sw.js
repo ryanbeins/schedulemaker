3f9fb399b23ecb69f5096cf9017e7e7331d6a180
Ryan Beins <ryanbeins@gmail.com>
2026-07-23 08:56:55 +0800

    Restyle duty warning banner with solid red background and icon badge
    Bump service worker cache version to v43 to push updated assets.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedulemaker-v42';
+const CACHE = 'schedulemaker-v43';
 const ASSETS = ['./home.html', './CSS/style.css', './JS/script.js', './JS/html2canvas.min.js', './manifest.json'];
 
 self.addEventListener('install', e => {
